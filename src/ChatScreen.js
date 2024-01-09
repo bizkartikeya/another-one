b77ab3a64b7e6b097d7c4f66c2f6e8d7f659a2c4
Kartikey Shrivastav <68100870+KartikeyKD@users.noreply.github.com>
2024-01-09 17:32:45 +0530

    ....

--- src/ChatScreen.js
+++ src/ChatScreen.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useRef } from "react";
 import LoadingBar from "react-top-loading-bar";
 
 function ChatScreen() {
-  const inputRef = useRef(null);
+  const inputRef = useRef(null); // null reference added
 
   const [progress, setProgress] = useState(0);
   const BaseUrl = "http://127.0.0.1:8000/";
