d539187cebb282690685e5f4cef9120eb84d411f
Kartikey Shrivastav <68100870+KartikeyKD@users.noreply.github.com>
2024-01-22 13:12:54 +0530

    ..

--- src/Login.js
+++ src/Login.js
@@ -31,7 +31,7 @@ function Login(props) {
 
   const handleEmailOnChange = (e) => {
     setInputEmailValue(e.target.value);
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
     setIsValidEmail(emailRegex.test(inputEmailValue));
   };
   const handlePasswordOnChange = (e) => {
